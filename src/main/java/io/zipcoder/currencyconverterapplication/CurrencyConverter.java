d54cbd3853156fa2d515deb90ebfc1e64241729d
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-26 17:47:08 -0400

    finished

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -2,6 +2,9 @@ package io.zipcoder.currencyconverterapplication;
 
 public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
+
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
     }
+
+
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -1,23 +1,27 @@
 package io.zipcoder.currencyconverterapplication;
 
+import io.zipcoder.currencyconverterapplication.currencies.UniversalCurrency;
+
 public enum CurrencyType {
-    AUSTRALIAN_DOLLAR(2.70),
-    CANADIAN_DOLLAR(2.64),
-    CHINESE_YR(13.84),
-    EURO(1.88),
-    FRANC(2.02),
-    POUND(1.64),
-    RINGGIT(8.94),
-    RUPEE(136.64),
-    SINGAPORE_DOLLAR(1.86),
-    US_DOLLAR(2.0),
-    UNIVERSAL_CURRENCY(1.0),
-    YEN(231.68);
+    AUSTRALIAN_DOLLAR(2.70, "AustralianDollar"),
+    CANADIAN_DOLLAR(2.64, "CanadianDollar"),
+    CHINESE_YR(13.84, "ChineseYR"),
+    EURO(1.88, "Euro"),
+    FRANC(2.02, "Franc"),
+    POUND(1.64, "Pound"),
+    RINGGIT(8.94, "Ringgit"),
+    RUPEE(136.64, "Rupee"),
+    SINGAPORE_DOLLAR(1.86, "SingaporeDollar"),
+    US_DOLLAR(2.0, "USDollar"),
+    UNIVERSAL_CURRENCY(1.0, "UniversalCurrency"),
+    YEN(231.68, "Yen");
 
     private final double rate;
+    private final String currencyName;
 
-    CurrencyType(double rate) {
+    CurrencyType(double rate, String currencyName) {
         this.rate = rate;
+        this.currencyName = currencyName;
     }
 
     public Double getRate() {
@@ -25,6 +29,10 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
-        return null;
-    }
-}
+        for (CurrencyType cur : CurrencyType.values()) {
+            if ( cur.currencyName.equals(currency.getClass().getSimpleName())){
+                return cur;
+            }
+        }
+     return null;
+}}
